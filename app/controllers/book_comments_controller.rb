e2f4c33fd8b645bfd643dc6d026cb811fd0b764f
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2023-05-08 16:49:25 +0000

    Initial Commit8

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,10 @@ class BookCommentsController < ApplicationController
     @comment = current_user.book_comments.new(book_comment_params)
     @comment.book_id = @book.id
     @comment.save
-    redirect_to book_path(@book)
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    BookComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
   end
 
 
